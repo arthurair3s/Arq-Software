cf0e942a3b8edafb91909b8b01b377eb37bd0394
arthurair3s <arthuraires0@gmail.com>
2026-03-25 19:09:43 -0300

    ajusta estrutura de pastas, cria microserviço, adiciona redis e ms-entregadores-cs, configura docker-compose e dockerfiles

--- primeira aula/ifood-api/src/entregador/Resolver/entregadorQuery.js
+++
@@ -1,6 +0,0 @@
-import * as entregadorService from '../entregadorService.js'
-
-export const Query = {
-  entregadores: async () => entregadorService.listar(),
-  entregador: async (_, { id }) => entregadorService.buscarPorId(id)
-}
